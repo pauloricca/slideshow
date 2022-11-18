23e0718947a22f922d9438aa157c74b633e3c6d1
pauloricca <pauloricca@gmail.com>
2022-11-18 14:23:21 +0000

    added instructions and create dir

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
 $imgPath = '/img/';
 
+if (!is_dir(__DIR__.$imgPath)) mkdir(__DIR__.$imgPath);
+
 function validImage($file)
 {
 	global $imgPath;
     return !is_dir(__DIR__.$imgPath.$file);
 }
 $files = scandir(__DIR__.$imgPath);
+
 shuffle($files);
 $imgs = [];
 foreach($files as $file) if($file != '.' && $file != '..') array_push($imgs, $file);
+
+if (count($imgs) == 0) die ("Place your images on the img directory.");
+
 ?>
 <html>
 	<head>
