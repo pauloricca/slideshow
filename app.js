90551eb995bf35bae9fc158dda4359d44df5c9e3
pauloricca <pauloricca@gmail.com>
2022-11-18 12:42:44 +0000

    updated animation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(() => {
     var current = 0;
     var interval = 1500;
     var showFor = 800;
-    var nColours = 7;
+    var nColours = 6;
 
     var showNext = () => {
         var img = $('[data-id="' + current + '"]');
@@ -12,9 +12,13 @@ $(() => {
         img.css('left', Math.random() * (screenWidth - img.width()));
         img.css('top', Math.random() * (screenHeight - img.height()));
 
+        var rotation = 20 * Math.random() - 10;
+        var translateX = 400 * Math.random() - 200;
+        var translateY = 0 * Math.random() - 0;
+        img.css('transform', 'rotate(' + rotation + 'deg) translate3d(' + translateX + 'px, ' + translateY + 'px, 0)');
         img.addClass('show');
         for(var i = 0; i < nColours; i++) img.removeClass('colour-' + i);
-        img.addClass('colour-' + Math.round(Math.random() * nColours));
+        img.addClass('colour-' + Math.floor(Math.random() * nColours));
         setTimeout(() => img.removeClass('show'), showFor);
 
         current++;
